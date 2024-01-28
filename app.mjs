0eac89a93f01a3f4228eb61748442768f7809636
kashio masaki <masaking20001010@gmail.com>
2024-01-28 23:04:01 +0900

    add: 素材アップ

---
+++ app.mjs
@@ -0,0 +1,89 @@
+import express from "express";
+import http from 'http';
+
+const app = express();
+
+app.get("/renderTutorial", (req, res) => res.type('html').send(html));
+
+const html = `
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Hello from Render!</title>
+    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
+    <script>
+      setTimeout(() => {
+        confetti({
+          particleCount: 100,
+          spread: 70,
+          origin: { y: 0.6 },
+          disableForReducedMotion: true
+        });
+      }, 500);
+    </script>
+    <style>
+      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
+      @font-face {
+        font-family: "neo-sans";
+        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
+        font-style: normal;
+        font-weight: 700;
+      }
+      html {
+        font-family: neo-sans;
+        font-weight: 700;
+        font-size: calc(62rem / 16);
+      }
+      body {
+        background: white;
+      }
+      section {
+        border-radius: 1em;
+        padding: 1em;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        margin-right: -50%;
+        transform: translate(-50%, -50%);
+      }
+    </style>
+  </head>
+  <body>
+    <section>
+      Hello from Render!
+    </section>
+  </body>
+</html>
+`
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+let helloMessage = "hello server";
+app.get('/hello',(req,res)=>{
+  res.send(helloMessage);
+});
+
+app.get('/id',(req,res)=>{
+  res.send("123456");
+});
+
+app.post('/post-hello',(req,res)=>{
+  if(req.body.message){
+    helloMessage = req.body.message;
+    res.send("OK:"+helloMessage);
+  }else{
+    res.send("ERROR");
+  }
+});
+
+let counter = 0;
+app.get('/countup',(req,res)=>{
+  counter ++;
+  res.send(""+counter);
+});
+
+const webServer = http.createServer(app);
+webServer.listen(3000,()=>{
+  console.log("server running PORT:"+3000);
+});
